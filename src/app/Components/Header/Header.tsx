3100e8c3e1ca24453d6682b354d573f8fcadacec
jonathanbernard265@gmail.com <jbw7@illinois.edu>
2023-12-24 00:36:21 +0700

    Header and Logo

---
+++ src/app/Components/Header/Header.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Image from 'next/image';
+
+const Header = () => {
+    return (
+        <header className = "py-10 px-4 container mx-auto text-xl flex flex-wrap md: flex-nowrap items-center justify-between">
+            <div className="flex items-center w-full md: 2/3">
+                <Link href="/">
+                    <Image src="/mainLogo.png" alt="Logo" width={200} height={100}/>
+                </Link>
+            </div>
+        </header>
+
+    );
+};
+
+export default Header;
