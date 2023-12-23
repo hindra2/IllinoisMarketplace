884d7e5d08d750578a26f3de74782fe3f4102caa
jonathanbernard265@gmail.com <jbw7@illinois.edu>
2023-12-24 03:20:31 +0700

    sidebar height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="flex pt-20">
+      <aside
+        className="fixed w-1/6 h-sideHeight bg-white rounded-2xl ml-3"
+        aria-label="Sidebar"
+      >
+        {/* Sidebar content goes here */}
+      </aside>
+      <main className="flex-1">{/* Main content goes here */}</main>
+    </div>
+  );
 }
