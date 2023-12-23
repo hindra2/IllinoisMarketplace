a4af2b34d2878d32aad49fccb06f0f72491f1263
jonathanbernard265@gmail.com <jbw7@illinois.edu>
2023-12-24 00:41:15 +0700

    Header and Logo

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
         <header className = "py-10 px-4 container mx-auto text-xl flex flex-wrap md: flex-nowrap items-center justify-between">
             <div className="flex items-center w-full md: 2/3">
                 <Link href="/">
-                    <Image src="/mainLogo.png" alt="UofILogo" width={200} height={100}/>
+                    <Image src="/mainLogo.png" alt="Logo" width={200} height={100}/>
                 </Link>
             </div>
         </header>
