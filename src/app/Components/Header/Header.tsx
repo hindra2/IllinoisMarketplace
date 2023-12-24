99e7213ffd7beb6cc215f773fe7c7802b04f28fa
jonathanbernard265@gmail.com <jbw7@illinois.edu>
2023-12-25 01:10:27 +0700

    changed icons

--- src/app/Components/Header/Header.tsx
+++ src/app/Components/Header/Header.tsx
@@ -17,42 +17,30 @@ const Header = () => {
       </div>
 
       <div className="flex-grow flex items-center mx-4 relative">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          className="w-8 h-8 absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+        <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+          <Image
+            src="/searchIcon.png"
+            alt="Search Icon"
+            width={24} // You might want to reduce the size so it fits within the input
+            height={20}
+            layout="fixed"
           />
-        </svg>
+        </div>
         <input
           type="search"
           placeholder="Search Marketplace"
-          className="w-full pl-12 pr-20 py-2 border border-gray-300 rounded-3xl focus:outline-none bg-white"
+          className="w-full pl-10 pr-20 py-3 border border-gray-300 rounded-3xl focus:outline-none bg-white" // Adjust pl value to match icon's width plus some padding
         />
       </div>
 
       <div className="flex items-center">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          className="w-11 h-11"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-          />
-        </svg>
+        <Image
+          src="/accountLogo.png"
+          alt="Illinois Logo"
+          width={46}
+          height={100}
+          layout="fixed"
+        />
       </div>
     </header>
   );
