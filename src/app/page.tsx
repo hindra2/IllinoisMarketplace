4c21e993197165ee0ae7339b93e49993b8e851be
jonathanbernard265@gmail.com <jbw7@illinois.edu>
2023-12-30 00:21:35 +0700

    product cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,13 @@ export default function Home() {
         className="fixed w-[256px] h-sideHeight bg-white rounded-2xl ml-[10px]"
         aria-label="Sidebar"
       >
+        <span className="absolute flex text-2xl ml-4 text-black mt-[350px]">
+          Price
+        </span>
+
         <span className="absolute flex text-2xl ml-4 text-black mt-[463px]">
           Categories
         </span>
-
         <div className="absolute flex items-center ml-3 mt-[500px]">
           <div className="w-12 h-12 bg-buttonBlue rounded-full flex justify-center items-center">
             <Image
@@ -72,7 +75,19 @@ export default function Home() {
       </aside>
       <main className="flex ml-[276px]">
         <div className="fixed rounded-2xl bg-white h-sideHeight w-5/6">
-          {/* Content of the rectangle container goes here */}
+          <div className="w-[274px] h-[416px] overflow-hidden shadow-lg ml-3 mt-3 rounded-xl">
+            <Image
+              src="/placeholders/macbook.jpg"
+              alt="Product Name"
+              width={274}
+              height={274}
+              objectFit="cover"
+            />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">Macbook Pro 16-inch</div>
+              <p className="text-gray-700 text-base">$499</p>
+            </div>
+          </div>
         </div>
       </main>
     </div>
